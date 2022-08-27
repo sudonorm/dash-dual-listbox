c6b3c32c4b11aaaed3b8d6d47cc9fd2bdd264a6a
sudonorm <sudonorm@protonmail.com>
2022-08-27 13:23:09 +0200

    added DualList.py and other files of dash_dual_listbox which have been recompiled and now work

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Install using:
 ************************************************************************
 <code>pip install dash-dual-listbox</code> installs a version that contains packages which no longer work. In that version, callbacks are broken.
 
-The DualList.py file will need to be rebuilt. 
+If you cloned the repo from here (https://github.com/vivekvs1/dash-dual-listbox.git), the DualList.py file will need to be recompiled. 
 
 To do this:
 
@@ -18,6 +18,9 @@ To do this:
 - npm run build:py
 
 You can now run: <code>python setup.py install</code> to install the dash-dual-listbox to your packages
+
+Alternatively, you can clone this repo and run <code>python setup.py install</code> to install the dash-dual-listbox to your packages. The DualList.py file in this fork of the original dash-dual-listbox repo is working (as of August 2022)
+
 
 To get the list box to show up properly styled:
 - copy the style.css file from <code>dash_dual_listbox/style.css</code> to an <code>assets</code> folder in your dash app
--- dash_dual_listbox/DualList.py
+++ dash_dual_listbox/DualList.py
@@ -8,65 +8,87 @@ class DualList(Component):
 
 
 Keyword arguments:
-- id (string; optional): The ID of this component, used to identify dash components
-in callbacks. The ID needs to be unique across all of the
-components in an app.
-- selected (list; required): List of selected options, will appear in the right box
-- available (list; required): List of available options, will appear in the left box
-- leftLabel (string; optional): A header for the left (available) list
-- rightLabel (string; optional): A header for the right (selected) list
-- searchable (boolean; optional): A false value will hide the search field on the top
-- sortable (boolean; optional): A false value will hide the reorder buttons on the right
-- moveLeftIcon (string; optional): fontawesome icons or icon of your choice
-- moveRightIcon (string; optional): fontawesome icons or icon of your choice
-- moveAllLeftIcon (string; optional): fontawesome icons or icon of your choice
-- moveAllRightIcon (string; optional): fontawesome icons or icon of your choice
-- moveUpIcon (string; optional): fontawesome icons or icon of your choice
-- moveTopIcon (string; optional): fontawesome icons or icon of your choice
-- moveDownIcon (string; optional): fontawesome icons or icon of your choice
-- moveBottomIcon (string; optional): fontawesome icons or icon of your choice
 
-Available events: """
+- id (string; optional):
+    The ID of this component, used to identify dash components  in
+    callbacks. The ID needs to be unique across all of the  components
+    in an app.
+
+- available (list of dicts; required):
+    List of available options, will appear in the left box.
+
+    `available` is a list of dicts with keys:
+
+    - label (string; required)
+
+    - value (boolean
+
+      Or number | string | dict | list; required) | dict with keys:
+
+    - options (list of dicts; optional)
+
+        `options` is a list of dicts with keys:
+
+        - label (string; required)
+
+        - value (boolean | number | string | dict | list; required)
+
+    - value (boolean | number | string | dict | list; optional)
+
+- leftLabel (string; optional):
+    A header for the left (available) list.
+
+- moveAllLeftIcon (string; optional):
+    fontawesome icons or icon of your choice.
+
+- moveAllRightIcon (string; optional):
+    fontawesome icons or icon of your choice.
+
+- moveBottomIcon (string; optional):
+    fontawesome icons or icon of your choice.
+
+- moveDownIcon (string; optional):
+    fontawesome icons or icon of your choice.
+
+- moveLeftIcon (string; optional):
+    fontawesome icons or icon of your choice.
+
+- moveRightIcon (string; optional):
+    fontawesome icons or icon of your choice.
+
+- moveTopIcon (string; optional):
+    fontawesome icons or icon of your choice.
+
+- moveUpIcon (string; optional):
+    fontawesome icons or icon of your choice.
+
+- rightLabel (string; optional):
+    A header for the right (selected) list.
+
+- searchable (boolean; optional):
+    A False value will hide the search field on the top.
+
+- selected (list of strings; required):
+    List of selected options, will appear in the right box.
+
+- sortable (boolean; optional):
+    A False value will hide the reorder buttons on the right."""
+    _children_props = []
+    _base_nodes = ['children']
+    _namespace = 'dash_dual_listbox'
+    _type = 'DualList'
     @_explicitize_args
     def __init__(self, id=Component.UNDEFINED, selected=Component.REQUIRED, available=Component.REQUIRED, leftLabel=Component.UNDEFINED, rightLabel=Component.UNDEFINED, searchable=Component.UNDEFINED, sortable=Component.UNDEFINED, moveLeftIcon=Component.UNDEFINED, moveRightIcon=Component.UNDEFINED, moveAllLeftIcon=Component.UNDEFINED, moveAllRightIcon=Component.UNDEFINED, moveUpIcon=Component.UNDEFINED, moveTopIcon=Component.UNDEFINED, moveDownIcon=Component.UNDEFINED, moveBottomIcon=Component.UNDEFINED, **kwargs):
-        self._prop_names = ['id', 'selected', 'available', 'leftLabel', 'rightLabel', 'searchable', 'sortable', 'moveLeftIcon', 'moveRightIcon', 'moveAllLeftIcon', 'moveAllRightIcon', 'moveUpIcon', 'moveTopIcon', 'moveDownIcon', 'moveBottomIcon']
-        self._type = 'DualList'
-        self._namespace = 'dash_dual_listbox'
+        self._prop_names = ['id', 'available', 'leftLabel', 'moveAllLeftIcon', 'moveAllRightIcon', 'moveBottomIcon', 'moveDownIcon', 'moveLeftIcon', 'moveRightIcon', 'moveTopIcon', 'moveUpIcon', 'rightLabel', 'searchable', 'selected', 'sortable']
         self._valid_wildcard_attributes =            []
-        self.available_events = []
-        self.available_properties = ['id', 'selected', 'available', 'leftLabel', 'rightLabel', 'searchable', 'sortable', 'moveLeftIcon', 'moveRightIcon', 'moveAllLeftIcon', 'moveAllRightIcon', 'moveUpIcon', 'moveTopIcon', 'moveDownIcon', 'moveBottomIcon']
+        self.available_properties = ['id', 'available', 'leftLabel', 'moveAllLeftIcon', 'moveAllRightIcon', 'moveBottomIcon', 'moveDownIcon', 'moveLeftIcon', 'moveRightIcon', 'moveTopIcon', 'moveUpIcon', 'rightLabel', 'searchable', 'selected', 'sortable']
         self.available_wildcard_properties =            []
-
         _explicit_args = kwargs.pop('_explicit_args')
         _locals = locals()
-        _locals.update(kwargs)  # For wildcard attrs
+        _locals.update(kwargs)  # For wildcard attrs and excess named props
         args = {k: _locals[k] for k in _explicit_args if k != 'children'}
-
-        for k in ['selected', 'available']:
+        for k in ['available', 'selected']:
             if k not in args:
                 raise TypeError(
                     'Required argument `' + k + '` was not specified.')
         super(DualList, self).__init__(**args)
-
-    def __repr__(self):
-        if(any(getattr(self, c, None) is not None
-               for c in self._prop_names
-               if c is not self._prop_names[0])
-           or any(getattr(self, c, None) is not None
-                  for c in self.__dict__.keys()
-                  if any(c.startswith(wc_attr)
-                  for wc_attr in self._valid_wildcard_attributes))):
-            props_string = ', '.join([c+'='+repr(getattr(self, c, None))
-                                      for c in self._prop_names
-                                      if getattr(self, c, None) is not None])
-            wilds_string = ', '.join([c+'='+repr(getattr(self, c, None))
-                                      for c in self.__dict__.keys()
-                                      if any([c.startswith(wc_attr)
-                                      for wc_attr in
-                                      self._valid_wildcard_attributes])])
-            return ('DualList(' + props_string +
-                   (', ' + wilds_string if wilds_string != '' else '') + ')')
-        else:
-            return (
-                'DualList(' +
-                repr(getattr(self, self._prop_names[0], None)) + ')')
